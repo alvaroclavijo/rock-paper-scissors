b12b58ec2d9ba3c49af1339a15e7170967f96c44
aclavijo91 <alvaro.clavijo.b21@gmail.com>
2021-08-16 21:53:58 -0400

    PC design

--- js/main.js
+++ js/main.js
@@ -13,37 +13,64 @@ var resultBox = document.getElementById("result-box");
 var resultText = document.getElementById("result-txt");
 var score = document.getElementById("actual-score");
 //mobile design 375px
-var x=window.matchMedia("(max-width: 376px)");
+var x = window.matchMedia("(max-width: 376px)");
 let houseButtons = ["paper", "rock", "scissors"];
 
 function reloadPage() {
-    houseSelection.style.display = "none";
-    mySelectionTitle.style.display = "none";
-    houseSelectionTitle.style.display = "none";
-    resultBox.style.display = "none";
-    gameContainer.style.backgroundImage = "url(./images/bg-triangle.svg)";
+    if (x.matches) {
+        houseSelection.style.display = "none";
+        mySelectionTitle.style.display = "none";
+        houseSelectionTitle.style.display = "none";
+        resultBox.style.display = "none";
+        gameContainer.style.backgroundImage = "url(./images/bg-triangle.svg)";
 
-    paperBtn.style.width = "110px";
-    paperBtn.style.height = "110px";
-    paperBtn.style.backgroundSize = "60px 60px";
-    paperBtn.style.left = "10px";
-    paperBtn.style.top = "40px";
+        paperBtn.style.width = "110px";
+        paperBtn.style.height = "110px";
+        paperBtn.style.backgroundSize = "60px 60px";
+        paperBtn.style.left = "10px";
+        paperBtn.style.top = "40px";
 
-    rockBtn.style.width = "110px";
-    rockBtn.style.height = "110px";
-    rockBtn.style.backgroundSize = "60px 60px";
-    rockBtn.style.left = "115px";
-    rockBtn.style.top = "276px";
+        rockBtn.style.width = "110px";
+        rockBtn.style.height = "110px";
+        rockBtn.style.backgroundSize = "60px 60px";
+        rockBtn.style.left = "115px";
+        rockBtn.style.top = "276px";
 
-    scissorsBtn.style.width = "110px";
-    scissorsBtn.style.height = "110px";
-    scissorsBtn.style.backgroundSize = "60px 60px";
-    scissorsBtn.style.left = "235px";
-    scissorsBtn.style.top = "40px";
+        scissorsBtn.style.width = "110px";
+        scissorsBtn.style.height = "110px";
+        scissorsBtn.style.backgroundSize = "60px 60px";
+        scissorsBtn.style.left = "235px";
+        scissorsBtn.style.top = "40px";
+    } else {
+        houseSelection.style.display = "none";
+        mySelectionTitle.style.display = "none";
+        houseSelectionTitle.style.display = "none";
+        resultBox.style.display = "none";
+        gameContainer.style.backgroundImage = "url(./images/bg-triangle.svg)";
+
+        paperBtn.style.width = "136px";
+        paperBtn.style.height = "136px";
+        paperBtn.style.backgroundSize = "70px 70px";
+        paperBtn.style.left = "110px";
+        paperBtn.style.top = "50px";
+
+        rockBtn.style.width = "136px";
+        rockBtn.style.height = "136px";
+        rockBtn.style.backgroundSize = "70px 70px";
+        rockBtn.style.left = "250px";
+        rockBtn.style.top = "286px";
+
+        scissorsBtn.style.width = "136px";
+        scissorsBtn.style.height = "136px";
+        scissorsBtn.style.backgroundSize = "70px 70px";
+        scissorsBtn.style.left = "375px";
+        scissorsBtn.style.top = "50px";
+    }
 
     rockBtn.style.display = "block";
     scissorsBtn.style.display = "block";
     paperBtn.style.display = "block";
+
 
 };
 function showRules() {
@@ -66,29 +93,29 @@ function randomButtons_paperSelected(min, max) {
                 houseSelection.appendChild(paperHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU TIED";
-                var a=Number(score.innerHTML)+0;
-                var b=a.toString();
-                score.innerHTML=b;
-                
+                var a = Number(score.innerHTML) + 0;
+                var b = a.toString();
+                score.innerHTML = b;
+
             } else if (button == "rock") {
                 var rockHouseBtn = document.createElement("div");
                 rockHouseBtn.setAttribute("id", "rock-housebtn");
                 houseSelection.appendChild(rockHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU WIN";
-                var a=Number(score.innerHTML)+1;
-                var b=a.toString();
-                score.innerHTML=b;
-                
+                var a = Number(score.innerHTML) + 1;
+                var b = a.toString();
+                score.innerHTML = b;
+
             } else if (button == "scissors") {
                 var scissorsHouseBtn = document.createElement("div");
                 scissorsHouseBtn.setAttribute("id", "scissors-housebtn");
                 houseSelection.appendChild(scissorsHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU LOSE";
-                var a=Number(score.innerHTML)-1;
-                var b=a.toString();
-                score.innerHTML=b;  
+                var a = Number(score.innerHTML) - 1;
+                var b = a.toString();
+                score.innerHTML = b;
             }
             houseSelection.style.display = "block";
 
@@ -107,27 +134,27 @@ function randomButtons_rockSelected(min, max) {
                 houseSelection.appendChild(paperHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU LOSE";
-                var a=Number(score.innerHTML)-1;
-                var b=a.toString();
-                score.innerHTML=b; 
+                var a = Number(score.innerHTML) - 1;
+                var b = a.toString();
+                score.innerHTML = b;
             } else if (button == "rock") {
                 var rockHouseBtn = document.createElement("div");
                 rockHouseBtn.setAttribute("id", "rock-housebtn");
                 houseSelection.appendChild(rockHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU TIED";
-                var a=Number(score.innerHTML)+0;
-                var b=a.toString();
-                score.innerHTML=b;
+                var a = Number(score.innerHTML) + 0;
+                var b = a.toString();
+                score.innerHTML = b;
             } else if (button == "scissors") {
                 var scissorsHouseBtn = document.createElement("div");
                 scissorsHouseBtn.setAttribute("id", "scissors-housebtn");
                 houseSelection.appendChild(scissorsHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU WIN";
-                var a=Number(score.innerHTML)+1;
-                var b=a.toString();
-                score.innerHTML=b;
+                var a = Number(score.innerHTML) + 1;
+                var b = a.toString();
+                score.innerHTML = b;
             }
             houseSelection.style.display = "block";
         }, 1200);
@@ -145,9 +172,9 @@ function randomButtons_scissorsSelected(min, max) {
                 houseSelection.appendChild(paperHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU WIN";
-                var a=Number(score.innerHTML)+1;
-                var b=a.toString();
-                score.innerHTML=b;
+                var a = Number(score.innerHTML) + 1;
+                var b = a.toString();
+                score.innerHTML = b;
             } else if (button == "rock") {
                 houseSelection.style.display = "block";
                 var rockHouseBtn = document.createElement("div");
@@ -155,9 +182,9 @@ function randomButtons_scissorsSelected(min, max) {
                 houseSelection.appendChild(rockHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU LOSE";
-                var a=Number(score.innerHTML)-1;
-                var b=a.toString();
-                score.innerHTML=b; 
+                var a = Number(score.innerHTML) - 1;
+                var b = a.toString();
+                score.innerHTML = b;
             } else if (button == "scissors") {
                 houseSelection.style.display = "block";
                 var scissorsHouseBtn = document.createElement("div");
@@ -165,21 +192,21 @@ function randomButtons_scissorsSelected(min, max) {
                 houseSelection.appendChild(scissorsHouseBtn);
                 resultBox.style.display = "block";
                 resultText.innerHTML = "YOU TIED";
-                var a=Number(score.innerHTML)+0;
-                var b=a.toString();
-                score.innerHTML=b;
+                var a = Number(score.innerHTML) + 0;
+                var b = a.toString();
+                score.innerHTML = b;
             }
 
         }, 1200);
 };
 function paperSelected() {
-    if(x.matches){
+    if (x.matches) {
         paperBtn.style.width = "110px";
         paperBtn.style.height = "110px";
         paperBtn.style.backgroundSize = "60px 60px";
         paperBtn.style.left = "10px";
         paperBtn.style.top = "40px";
-    }else{
+    } else {
         paperBtn.style.width = "200px";
         paperBtn.style.height = "200px";
         paperBtn.style.backgroundSize = "120px 120px";
@@ -195,13 +222,13 @@ function paperSelected() {
     randomButtons_paperSelected(0, 2);
 }
 function rockSelected() {
-    if(x.matches){
+    if (x.matches) {
         rockBtn.style.width = "110px";
         rockBtn.style.height = "110px";
         rockBtn.style.backgroundSize = "60px 60px";
         rockBtn.style.left = "10px";
         rockBtn.style.top = "40px";
-    }else{
+    } else {
         rockBtn.style.width = "200px";
         rockBtn.style.height = "200px";
         rockBtn.style.backgroundSize = "120px 120px";
@@ -216,13 +243,13 @@ function rockSelected() {
     randomButtons_rockSelected(0, 2);
 }
 function scissorsSelected() {
-    if(x.matches){
+    if (x.matches) {
         scissorsBtn.style.width = "110px";
         scissorsBtn.style.height = "110px";
         scissorsBtn.style.backgroundSize = "60px 60px";
         scissorsBtn.style.left = "10px";
-        scissorsBtn.style.top = "40px"; 
-    }else{
+        scissorsBtn.style.top = "40px";
+    } else {
         scissorsBtn.style.width = "200px";
         scissorsBtn.style.height = "200px";
         scissorsBtn.style.backgroundSize = "120px 120px";
